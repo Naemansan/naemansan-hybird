040212d5c0da1e7952cd1456541c5f6e30bb335b
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2023-12-05 13:31:21 +0900

    :lipstick: Style: color set setting
    style: color set setting

--- lib/utilities/style/color_styles.dart
+++ lib/utilities/style/color_styles.dart
@@ -1,19 +1,28 @@
 import 'package:flutter/material.dart';
 
 class ColorStyles {
-  static const Color main = Color(0xFF2666F6);
-  static const Color sub1 = Color(0xFF5588FD);
-  static const Color sub2 = Color(0xFFC1D2FF);
-  static const Color sub3 = Color(0xFFF1F5FE);
+  // main Color
+  static const Color main1 = Color(0xFF68A37B);
+  static const Color main2 = Color(0xFF4F7D5E);
+
+  // black n white
   static const Color black = Color(0xFF151515);
-  static const Color gray6 = Color(0xFF464655);
-  static const Color gray5 = Color(0xFF626272);
-  static const Color gray4 = Color(0xFF90909F);
-  static const Color gray3 = Color(0xFFC6C6CF);
-  static const Color gray2 = Color(0xFFE9E9EE);
-  static const Color gray1 = Color(0xFFF5F5F9);
   static const Color white = Color(0xFFFFFFFF);
-  static const Color green = Color(0xFF63DC68);
-  static const Color yellow = Color(0xFFFFC100);
-  static const Color red = Color(0xFFFB5D5D);
+
+  // gray
+  static const Color gray6 = Color(0xFF464655);
+  static const Color gray5 = Color(0xFF3B3B47);
+  static const Color gray4 = Color(0xFF585865);
+  static const Color gray3 = Color(0xFF737381);
+  static const Color gray2 = Color(0xFF9393A0);
+  static const Color gray1 = Color(0xFFD6D6DD);
+  static const Color gray0 = Color(0xFFF4F6FA);
+
+  // emotion Color
+  static const Color emotionJoy = Color(0xFFFFE2B6);
+  static const Color emotionCom = Color(0xFFFFDF81);
+  static const Color emotionSur = Color(0xFFD4DD81);
+  static const Color emotionSad = Color(0xFF93B5B5);
+  static const Color emotionBoolan = Color(0xFFAFC5D1);
+  static const Color emotionHurt = Color(0xFFD1E0E5);
 }
