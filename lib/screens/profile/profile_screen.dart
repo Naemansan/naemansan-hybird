a4c33ebdea190820e2a0abea4d45d3374957aed4
심서현 <simseohyun@simseohyeon-ui-MacBookAir.local>
2023-12-07 01:08:27 +0900

    :lipstick: Style: add FontStyle
    폰트 스타일 7개 추가했습니다.

--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:naemansan/utilities/style/color_styles.dart';
+import 'package:naemansan/utilities/style/font_styles.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
@@ -6,9 +8,34 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Profile"),
+      appBar: AppBar(
+        title: Text("마이페이지"),
+        backgroundColor: ColorStyles.white,
       ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(0, 12, 0, 32),
+        child: Column(children: [UserProfile()]),
+      ),
+    );
+  }
+}
+
+class UserProfile extends StatelessWidget {
+  const UserProfile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: ColorStyles.gray3,
+      ),
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      child: Column(children: [
+        Text(
+          "프로필",
+          style: FontStyles.semiBold20.copyWith(color: ColorStyles.black),
+        )
+      ]),
     );
   }
 }
