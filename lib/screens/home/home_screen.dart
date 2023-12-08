7f228fcbc58c73c85af2108a79f8017cf207969b
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2023-12-08 19:52:40 +0900

    :bulb: Cmt: home
    cmt: home

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -11,10 +11,11 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // 홈 화면에 HomeViewModel 연결
+    // HomeViewModel과 CourseController를 초기화한다.
     final HomeViewModel homeViewModel = Get.put(HomeViewModel());
     final CourseController courseController = Get.put(CourseController());
 
+    // 스크롤 이벤트 리스너를 설정한다. 현재 가장 보이는 코스 카드에 따라 지도의 경로를 업데이트한다.
     homeViewModel.scrollController.addListener(() {
       int mostVisibleIndex = homeViewModel.getMostVisibleCardIndex(
           courseController.course.value!.courses.length);
@@ -27,11 +28,14 @@ class HomeScreen extends StatelessWidget {
       body: GetBuilder<HomeViewModel>(
         init: homeViewModel,
         builder: (viewModel) {
+          // 로딩 중일 때 로딩 인디케이터를 표시한다.
           if (viewModel.isLoading) {
             return const Center(child: CircularProgressIndicator());
           }
+          // 스택 레이아웃을 사용하여 NaverMap과 코스 목록을 표시한다.
           return Stack(
             children: [
+              // NaverMap 위젯을 설정한다. HomeViewModel에서 제공하는 데이터를 사용하여 지도를 구성한다.
               Obx(() => NaverMap(
                     scrollGestureEnable: false,
                     zoomGestureEnable: false,
@@ -44,23 +48,26 @@ class HomeScreen extends StatelessWidget {
                     ),
                     initLocationTrackingMode: LocationTrackingMode.values[1],
                   )),
+              // 코스 목록을 수평 스크롤 형태로 표시한다.
               Positioned(
                 bottom: 20,
                 left: 16,
                 right: 0,
                 child: Obx(
                   () {
+                    // 코스 데이터를 로딩 중일 경우 로딩 인디케이터를 표시한다.
                     if (courseController.isLoading.value) {
                       return const Center(child: CircularProgressIndicator());
                     }
                     final courses = courseController.getCourse;
+                    // 코스 데이터가 없을 경우 안내 메시지를 표시한다.
                     if (courses == null) {
                       return Container(
                         color: Colors.white,
                         child: const Text("코스가 없습니다."),
                       );
                     }
-
+                    // 코스 카드를 리스트로 표시한다. 각 카드는 CourseCardWidget으로 구성된다.
                     return SizedBox(
                       height: 120,
                       child: ListView.separated(
@@ -71,7 +78,7 @@ class HomeScreen extends StatelessWidget {
                             width: 320,
                             child: CourseCardWidget(
                                 onCourseTap: (id) {
-                                  // 코스 선택 시 코스 화면으로 이동
+                                  // 코스 카드 선택 시 해당 코스 화면으로 이동한다.
                                   Get.toNamed("/course/$id");
                                 },
                                 course: courses.courses[index]),
