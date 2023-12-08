2dc6f82ca813802dab9739977d290514ba3021de
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2023-12-08 18:59:00 +0900

    :sparkles: Feat: near Course Widget
    feat: near Course Widget

---
+++ lib/widget/home/course_start_btn_widget.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:naemansan/utilities/style/color_styles.dart';
+import 'package:naemansan/utilities/style/font_styles.dart';
+import 'package:naemansan/viewModel/home_view_model.dart';
+
+class CourseStartBtnWidget extends StatelessWidget {
+  final HomeViewModel viewModel = Get.find();
+  CourseStartBtnWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        // CourseWalking 화면으로 이동
+        Get.toNamed('/course/walking');
+      },
+      child: Container(
+        height: 112,
+        decoration: const BoxDecoration(
+          color: ColorStyles.main1,
+          borderRadius: BorderRadius.all(
+            Radius.circular(4),
+          ),
+          // border color
+        ),
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(
+            24,
+            20,
+            24,
+            20,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text(
+                '산책 시작하기',
+                style: FontStyles.semiBold20.copyWith(
+                  color: ColorStyles.white,
+                ),
+              ),
+              Text(
+                viewModel.sigudongData?.toString() ?? '',
+                style: FontStyles.regular16.copyWith(
+                  color: ColorStyles.white,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
